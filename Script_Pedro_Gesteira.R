bb0cac5ad3ce25995bf08410a61c63c36a05feb7
Pedro Gesteira <pedro06g@gmail.com>
2021-11-09 20:56:38 -0300

    Parte da base de população terminada.
    Tentativa de transposição do mesmo código para a parte de mortalidade. Mas retorna erro na linha 70, na função str_detect

--- Script_Pedro_Gesteira.R
+++ Script_Pedro_Gesteira.R
@@ -19,24 +19,60 @@ pCadastradas <- as_tibble(pCadastradas[4:5493,]) %>% separate(col = 1,into = col
 
 lpops <- list()
 
-arquivos <- list.files(paste(getwd(),"/FE",sep = ""), pattern = "PopFE", full.names = TRUE, include.dirs = TRUE)
+arquivos.pop <- list.files(paste(getwd(),"/FE",sep = ""), pattern = "PopFE", full.names = TRUE, include.dirs = TRUE)
 
 for (i in seq_along(list.files(paste(getwd(),"/FE",sep = ""), pattern = "PopFE", full.names = TRUE, include.dirs = TRUE))) {
 
-  Pop <- read.csv(arquivos[i], sep = "\t")
+  Pop <- read.csv(arquivos.pop[i], sep = "\t")
   
-  cols <- unlist(str_split(Pop[which(str_detect(Pop[,1], "^Município")),], pattern = ";"))
+  cols.pop <- unlist(str_split(Pop[which(str_detect(Pop[,1], "^Município")),], pattern = ";"))
   
-  ano <- substr(arquivos[i],85,88)
+  cols.pop[-1] <- paste("Pop", cols.pop[-1], sep = "_")
+  
+  ano <- substr(arquivos.pop[i],85,88)
   
   Pop <- as.tibble(select(Pop,1)) %>% slice((which(str_detect(Pop[,1], "^Município"))+1):(which(str_detect(Pop[,1], "^Total"))-1)) 
   
-  Pop <- separate(Pop, col = 1,into = cols, sep = ";") %>% 
+  Pop <- separate(Pop, col = 1,into = cols.pop, sep = ";") %>% 
     separate(col = 1, into = c("M_cod","M_name"), sep = "\\s", extra = "merge")
   
   Pop <- Pop %>% pivot_longer(3:ncol(Pop), names_to = "FE",values_to = ano, values_drop_na = FALSE)
   
   lpops[[i]] <- Pop
 
+}
+
+Pop <- lpops[[1]]
+
+for (i in seq_along(lpops)) {
+  Pop <- left_join(Pop,lpops[[i+1]], by = c("M_cod", "M_name"))
+}
+
+Pop <- Pop %>% pivot_longer(4:ncol(Pop), names_to = "Ano", values_to = "Valor", values_drop_na = FALSE) %>% 
+  pivot_wider(names_from = "FE", values_from = "Valor") 
+
+#colnames(Pop)[4:14] <- paste("Pop",colnames(Pop[4:14]), sep ="_") #Não vou precisar graças a alteração na linha 30
+
+##Mortalidade - Total e por faixa etária
+
+lmorts <- list()
+
+arquivos.mort <- list.files(paste(getwd(),"/Mortalidade",sep = ""), pattern = "Mort", full.names = TRUE, include.dirs = TRUE)
+
+for (i in seq_along(list.files(paste(getwd(),"/Mortalidade",sep = ""), pattern = "Mort", full.names = TRUE, include.dirs = TRUE))) {
+  
+  Mort <- read.csv(arquivos.mort[i], sep = "\t")
+  
+  cols <- unlist(str_split(Mort[which(str_detect(Mort[,1], "^Município")),], pattern = ";"))
+  
+  ano <- substr(arquivos.mort[i],93,96)
+  
+  Mort <- as.tibble(select(Mort,1)) %>% slice((which(str_detect(Mort[,1], "^Município"))+1):(which(str_detect(Mort[,1], "^Total"))-1)) 
+  
+  Mort <- separate(Mort, col = 1,into = cols, sep = ";") %>% 
+    separate(col = 1, into = c("M_cod","M_name"), sep = "\\s", extra = "merge")
+  
+  lmorts[[i]] <- Mort
+  
 }
 
